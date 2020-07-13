cf78dae0724425bd0563f9c93d383a939687ef75
Guilherme Imamura Fernandes <gimamura@everis.com>
2020-07-13 19:24:33 -0300

    Implementing layout and animations

--- src/frontend/gremling/src/app/app.component.ts
+++ src/frontend/gremling/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, ViewChild, ElementRef } from '@angular/core';
+import { faTint, faLightbulb } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gremling';
+  faTint = faTint;
+  faLightbulb = faLightbulb;
+
+  constructor(private renderer:Renderer2) {}
+  @ViewChild('gremlinsArea', { static: false }) d1: ElementRef;
+
+  agua() {
+    console.log('ÁGUA')
+    const stage = this.renderer.createElement('div');
+    const img = this.renderer.createElement('img');
+
+    this.renderer.addClass(stage, 'stage');
+
+    this.renderer.setStyle(stage, 'margin-top', Math.floor(Math.random() * 60)+'vh');
+    this.renderer.setStyle(stage, 'margin-left', Math.floor(Math.random() * 88)+'%');
+
+    this.renderer.addClass(img, 'gremlin');
+    this.renderer.addClass(img, 'bounce');
+
+    this.renderer.setAttribute(img, 'src', '../assets/gremlin.png');
+  
+    this.renderer.appendChild(stage, img);
+    this.renderer.appendChild(this.d1.nativeElement, stage);
+  }
+
+  luz() {
+    console.log('LUZ')
+  }
 }
